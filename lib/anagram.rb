d987da4c8ec433f578c0ee49d3ec3a821be8acba
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-14 18:51:17 -0500

    commit tree 2501af03ffc2b2f5f7e5b99ddfd9e11bdd65382f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    # attr_accessor
+    def initialize(word)
+        @word = word
+    end
+    def match(list_of_words)
+        anagrams = []
+        list_of_words.each do |w|
+            # @word = @word.split("").sort
+            # binding.pry
+            # w = w.split("").sort
+            # binding.pry
+            anagrams = anagrams << w if w.split("").sort == @word.split("").sort
+        end
+        anagrams    
+    end
+end
+
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana listen))
